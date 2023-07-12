b476db49a8c5a615fcabda468bb5ffb90faf04e4
Adan <adanreh.m@gmail.com>
2023-07-12 20:59:14 +0200

    client deployed

--- fcrow/contracts/CrowdFunding.sol
+++ fcrow/contracts/CrowdFunding.sol
@@ -7,21 +7,20 @@ contract CrowdFunding {
         string title;
         string description;
         uint256 target;
-        uint256 deadlin;
+        uint256 deadline;
         uint256 amountCollected;
         string image;
         address[] donators;
         uint256[] donations;
     }
 
-    mapping( uint256 => Campaign) public campaigns;
+    mapping(uint256 => Campaign) public campaigns;
 
     uint256 public numberOfCampaigns = 0;
 
     function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
         Campaign storage campaign = campaigns[numberOfCampaigns];
 
-        //
         require(campaign.deadline < block.timestamp, "The deadline should be a date in the future.");
 
         campaign.owner = _owner;
@@ -34,28 +33,29 @@ contract CrowdFunding {
 
         numberOfCampaigns++;
 
-        return numberOfCampaigns - 1
+        return numberOfCampaigns - 1;
     }
 
-    function donateToCampaign(uint256 _id) public payable {  
-        uint256 ammount = msg.value;
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
 
         Campaign storage campaign = campaigns[_id];
 
         campaign.donators.push(msg.sender);
         campaign.donations.push(amount);
 
-        (bool sent,) = payable(campaign.owner).call{calue: amount}("");
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
 
         if(sent) {
             campaign.amountCollected = campaign.amountCollected + amount;
         }
     }
-    function getDonators(uint256 _id) view public return(address[] memory, uint256[] memory) {
-        return (campaign[_id].donators, campaigns[_id].donations);
+
+    function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
     }
 
-    function getCampaigns() public view return (Campaign[] memory) {
+    function getCampaigns() public view returns (Campaign[] memory) {
         Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
 
         for(uint i = 0; i < numberOfCampaigns; i++) {
@@ -63,7 +63,7 @@ contract CrowdFunding {
 
             allCampaigns[i] = item;
         }
+
         return allCampaigns;
     }
-
 }
