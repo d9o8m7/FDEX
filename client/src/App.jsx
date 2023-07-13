bf5597e6a5985a12de9d7b91fc77099cb250f2fc
Adan <adanreh.m@gmail.com>
2023-07-13 15:40:17 +0200

    app working

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <div>Porque no funciona?</div>
+    </div>
+  );
+};
+
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 
-import {App} from './App';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-	<ThirdwebProvider desieredChainId={ChainId.Goerli}>
-	<Router>
-		<App />
-	</Router>
-	</ThirdwebProvider>
-)
+  <ThirdwebProvider desieredChainId={ChainId.Goerli}>
+    <Router>
+      <App />
+    </Router>
+  </ThirdwebProvider>
+);
